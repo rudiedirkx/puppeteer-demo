a93bac9c6fecd269cbba5eb6e00c83eecd63b992
Rudie Dirkx <github@hotblocks.nl>
2017-10-13 19:14:47 +0200

    good nav!

--- br.js
+++ br.js
@@ -2,7 +2,7 @@ const cfg = require('./env.js');
 const puppeteer = require('puppeteer');
 
 function pageUrl(page) {
-	console.log('    <= ' + page.url());
+	console.log(' => ' + page.url());
 	// console.log('');
 }
 
@@ -21,34 +21,38 @@ async function pageContent(page) {
 (async () => {
 	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
+	page.setViewport({
+		width: 1400,
+		height: 800,
+	});
 
 	page.on('request', req => {
-		console.log('  => ' + req.url);
-		// console.log('');
+		console.log('[request] ' + req.url);
 	});
 
-	page.on('load', e => {
-		// screenshot(page);
+	page.on('load', async () => {
+		console.log('[load] ' + page.url());
+		await screenshot(page);
 	});
 
-	page.on('framenavigated', frame => {
-		if (frame == page.mainFrame()) {
-			setTimeout(() => {
-				console.log(frame.url());
-				screenshot(page);
-			}, 1500);
-		}
-	});
+	// page.on('framenavigated', frame => {
+	// 	if (frame == page.mainFrame()) {
+	// 		setTimeout(() => {
+	// 			console.log(frame.url());
+	// 			screenshot(page);
+	// 		}, 100);
+	// 	}
+	// });
 
-	page.on('response', rsp => {
-		// screenshot(page);
-		// console.log('  => ' + rsp.request().url);
-		// console.log('');
-	});
+	// page.on('response', rsp => {
+	// 	// screenshot(page);
+	// 	// console.log('  => ' + rsp.request().url);
+	// 	// console.log('');
+	// });
 
 	await page.goto(cfg.baseUrl);
 
-pageUrl(page);
+// pageUrl(page);
 
 	await page.evaluate(function() {
 		document.querySelector('#login').focus();
@@ -59,15 +63,22 @@ pageUrl(page);
 // await screenshot(page);
 
 	await page.click('#login');
+	await page.waitForNavigation();
 
-pageUrl(page);
+// pageUrl(page);
 // await screenshot(page);
 
 	await page.goto(cfg.baseUrl + 'blockreservations');
-pageUrl(page);
+	// await page.waitForNavigation();
+// pageUrl(page);
+
+	var el = await page.$('a[href="/blockreservations/new"]');
+	await page.evaluate(el => el.click(), el);
+	// await el.click();
+	await page.waitForNavigation();
 
 
 
-
-	// setTimeout(async x => await browser.close(), 1000);
+	// await browser.close();
+	setTimeout(async x => await browser.close(), 1000);
 })();
